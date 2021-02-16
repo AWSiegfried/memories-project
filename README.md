c593bc0eb0db8e0726c9df15232c2eaf77fce0be
AWSiegfried <awsiegfried@gmail.com>
2021-02-16 11:56:45 -0800

    added crud functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # memories-project
+technologies used:
+MongoDB
+Express
+React
+Node
+Redux
+Material UI
--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js"
+dotenv.config();
 
 const app = express();
 
@@ -14,11 +16,9 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://awsiegfried:Bootcamp2020@myfirstconnection.o76xj.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, {
+ 
+mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
